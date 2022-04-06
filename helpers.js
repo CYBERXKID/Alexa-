999302956b5b7682905e22f5b64e5bbdfd915749
CYBERXKID <102310990+CYBERXKID@users.noreply.github.com>
2022-04-06 22:20:34 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *alexa*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *alexa*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *alexa*:  ```" + msg + "```"
 }
 
 
